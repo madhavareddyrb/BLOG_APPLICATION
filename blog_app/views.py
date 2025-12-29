00b614b39646acd94deaf9bc3a9e8cc4affe16c2
madhavareddyrb <madhavareddyrb@gmail.com>
2025-12-29 20:38:40 +0530

    fetching blogs by category in sepertae template

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import *
 # Create your views here.
+def blogs_by_category(request, category_id):
+  blog_category = Blog.objects.filter(status= 'published', category = category_id)
+  try:
+    category = Category.objects.get(pk=category_id,status='published')
+  except:
+    return redirect('home')
+  #category = get_object_or_404(Category, pk=category_id)
+  context = {
+    'blog_category' : blog_category,
+    'category' : category,
+  }
+
+  return render(request, 'posts_by_category.html', context)
